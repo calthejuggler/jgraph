a9a479837cfb1e0d5ad522063b6d194c3eef982d
Cal Courtney <calthejuggler@gmail.com>
2026-02-22 11:13:59 +0000

    dev(frontend): build in production mode

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { queryClient } from "@/lib/query-client";
 
 export const Route = createRootRoute({
