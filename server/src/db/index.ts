2ec27598befcc99e5424a0dc037394bdbad316ee
Cal Courtney <calthejuggler@gmail.com>
2026-02-22 10:22:37 +0000

    dev: run migrations on server start

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import * as schema from "./schema";
 
@@ -8,3 +9,7 @@ const connectionString =
 
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
+
+export async function runMigrations() {
+  await migrate(db, { migrationsFolder: "./drizzle" });
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,33 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { auth } from "./lib/auth";
+import { runMigrations } from "./db";
 import { loggingPlugin } from "./lib/logging";
 import { logger } from "./lib/logger";
 import { routes } from "./routes";
 
-const app = new Elysia()
-  .use(cors({ origin: Bun.env.CORS_ORIGIN ?? "http://localhost:5173", credentials: true }))
-  .use(loggingPlugin)
-  .use(routes)
-  .mount(auth.handler)
-  .get("/", () => "Hello Elysia")
-  .listen(3000);
+const main = async () => {
+  try {
+    await runMigrations();
+    logger.info({ event: "migrations_applied" });
+  } catch (err) {
+    logger.error({ event: "migrations_failed", error: err });
+    process.exit(1);
+  }
 
-logger.info({
-  event: "server_started",
-  host: app.server?.hostname,
-  port: app.server?.port,
-});
+  const app = new Elysia()
+    .use(cors({ origin: Bun.env.CORS_ORIGIN ?? "http://localhost:5173", credentials: true }))
+    .use(loggingPlugin)
+    .use(routes)
+    .mount(auth.handler)
+    .get("/", () => "Hello Elysia")
+    .listen(3000);
+
+  logger.info({
+    event: "server_started",
+    host: app.server?.hostname,
+    port: app.server?.port,
+  });
+};
+
+main();
